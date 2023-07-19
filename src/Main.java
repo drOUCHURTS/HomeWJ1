b4bbc681d2a46b12fb78bb17eae1474d4b0ca7bb
drOUCHURTS <127418512+drOUCHURTS@users.noreply.github.com>
2023-07-19 14:26:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int TicketPrise = 13676;
+        int ticketPrise = 13676;
         // Стоимость билета
-        int BonusPerPrise = 20;
+        int bonusPerPrise = 20;
         // Бонус начисляется за каждые 20 рублей потраченные на билет
-        int FinalBonus = TicketPrise / BonusPerPrise;
+        int finalBonus = ticketPrise / bonusPerPrise;
         // Расчет бонуса от стоимости билета
 
-        System.out.println("Количество бонусов киента: " + FinalBonus);
+        System.out.println("Количество бонусов киента: " + finalBonus);
     }
 }
